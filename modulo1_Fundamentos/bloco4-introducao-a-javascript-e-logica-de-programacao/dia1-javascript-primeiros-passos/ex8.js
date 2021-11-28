92be6534c21f1ff8a87c7db604ed94b01b665904
Igormcf <igormaciel82@gmail.com>
2021-11-28 14:56:38 -0300

    Adiciona exemplos do 7 ao 11

---
+++ modulo1_Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-javascript-primeiros-passos/ex8.js
@@ -0,0 +1,11 @@
+const a = 13;
+const b = 17;
+const c = 22;
+
+let isEven = false;
+
+if ((a % 2 === 0) || (b % 2 === 0) || (c % 2 === 0)) {
+    isEven = true;
+} 
+
+console.log(isEven);
---
+++ modulo1_Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-javascript-primeiros-passos/ex9.js
@@ -0,0 +1,10 @@
+const a = 84;
+const b = 24;
+const c = 74;
+
+let isOdd = false;
+
+if ((a % 2 !== 0) || (b % 2 !== 0) || (c % 2 !== 0)) {
+    isOdd = true;
+}
+console.log(isOdd);
