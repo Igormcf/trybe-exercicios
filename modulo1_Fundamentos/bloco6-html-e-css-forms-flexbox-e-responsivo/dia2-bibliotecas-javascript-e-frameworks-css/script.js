f0083209b6eb5e6add0b7a274f16e5cd74508b0b
Igormcf <igormaciel82@gmail.com>
2022-01-05 22:42:19 -0300

    Adiciona exercicio 1

---
+++ modulo1_Fundamentos/bloco6-html-e-css-forms-flexbox-e-responsivo/dia2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -0,0 +1,18 @@
+function criarOptions() {
+    const estado = document.getElementById('estados');
+    let siglas = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+
+    for (let index = 0; index < siglas.length; index += 1) {
+        let opcao = document.createElement('option');
+        opcao.innerText = siglas[index];
+        opcao.value = siglas[index];
+        estado.appendChild(opcao);
+    };
+};
+
+window.onload = function() {
+    criarOptions();
+
+    const botaoSub = document.querySelector('.submeter');
+    botaoSub.addEventListener('click', lidarComSub)
+};
