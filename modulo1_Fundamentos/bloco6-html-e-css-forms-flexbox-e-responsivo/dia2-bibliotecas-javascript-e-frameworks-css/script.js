5a11de03a19dc8e365b1243fbd668148cb92982d
Igormcf <igormaciel82@gmail.com>
2022-01-07 23:17:38 -0300

    Adiciona validaçoes

--- modulo1_Fundamentos/bloco6-html-e-css-forms-flexbox-e-responsivo/dia2-bibliotecas-javascript-e-frameworks-css/script.js
+++ modulo1_Fundamentos/bloco6-html-e-css-forms-flexbox-e-responsivo/dia2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -1,3 +1,5 @@
+
+
 function criarOptions() {
     const estado = document.getElementById('estados');
     let siglas = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
@@ -9,14 +11,81 @@ function criarOptions() {
         estado.appendChild(opcao);
     };
 };
-
+criarOptions();
+/* 
 document.getElementById('date').DatePickerX.init({
     mondayFirst: false,
     format: 'dd/mm/yyyy',
     required: true,
     messenger: 'Campo obrigatório',
-});
+}); */
 
-window.onload = function() {
-    criarOptions();
-};
+const validation = new JustValidate('#form'); 
+
+validation
+	.addField('#name', [
+		{
+			rule: 'required',
+			errorMessage: 'Nome obrigatório!'
+		}
+	])
+	.addField('#end', [
+		{
+			rule: 'required',
+			errorMessage: 'Email obrigatório!'
+		},
+		{
+      rule: 'email',
+      errorMessage: 'Email inválido!',
+    },
+	])
+	.addField('#doc', [
+		{
+			rule: 'required',
+			errorMessage: 'CPF obrigatório!'
+		}
+	])
+	.addField('#rua', [
+		{
+			rule: 'required',
+			errorMessage: 'Endereço obrigatório!'
+		}
+	])
+	.addField('#city', [
+		{
+			rule: 'required',
+			errorMessage: 'Cidade obrigatória!'
+		}
+	])
+	.addField('#estados', [
+		{
+			rule: 'required',
+			errorMessage: 'Selecione um Estado!'
+		}
+	])
+	.addRequiredGroup(
+    '#check-tipe',
+    'VocÊ deve selecionar um tipo de residência!'
+  )
+	.addField('#resume', [
+    {
+      validator: (value) => {
+        return value[0] === '!';
+      },
+			errorMessage: 'Campo obrigatório!'
+    },
+  ])
+	.addField('#cargo', [
+		{
+			rule: 'required',
+			errorMessage: 'Campo obrigatório!'
+		}
+	])
+	.addField('#description', [
+		{
+			rule: 'required',
+			errorMessage: 'Campo obrigatório!'
+		}
+	])
+
+
