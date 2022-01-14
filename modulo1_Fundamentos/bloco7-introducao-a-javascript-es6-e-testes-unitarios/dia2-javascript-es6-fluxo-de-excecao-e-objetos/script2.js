d75cabfe7cbf50c7e162cf8091b40fe843e49835
Igormcf <igormaciel82@gmail.com>
2022-01-13 21:55:56 -0300

    Adiciona exercicio 2 da parte 2

--- modulo1_Fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia2-javascript-es6-fluxo-de-excecao-e-objetos/script2.js
+++ modulo1_Fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia2-javascript-es6-fluxo-de-excecao-e-objetos/script2.js
@@ -40,3 +40,52 @@ return `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order['name
 }  
 console.log(obj());
 
+
+const obj2 = () => {
+
+  const order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        marguerita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+          price: 20,
+        }
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        }
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+        price: 5,
+      }
+    },
+    payment: {
+      total: 60,
+    },
+  };
+
+  order.name = 'Igor Maciel';
+  order.payment.total = 70;
+  const pizzas = Object.keys(order.order.pizza);
+  
+  /* Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."
+  Modifique o nome da pessoa compradora.
+  Modifique o valor total da compra para R$ 50,00. */
+  return `Olá ${order.name}, o total do seu pedido de ${pizzas[0]}, ${pizzas[1]} e ${order.order.drinks.coke.type} é R$ ${order.payment.total},00.`
+}  
+console.log(obj2());
