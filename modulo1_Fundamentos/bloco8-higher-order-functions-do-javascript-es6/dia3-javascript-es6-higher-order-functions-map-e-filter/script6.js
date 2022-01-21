e912e9b9d3185880dfe6ea26261a2c35d515eb20
Igormcf <igormaciel82@gmail.com>
2022-01-20 21:18:31 -0300

    Ends exercise 6

--- modulo1_Fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia3-javascript-es6-higher-order-functions-map-e-filter/script6.js
+++ modulo1_Fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia3-javascript-es6-higher-order-functions-map-e-filter/script6.js
@@ -62,3 +62,25 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+//6 - Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
+const expectedResult = [
+  'O Senhor dos Anéis',
+  'Fundação',
+  'O Chamado de Cthulhu',
+];
+
+function oldBooks() {
+  // escreva seu código aqui
+  const arrBooks = books.filter((item) => (2022 - item.releaseYear >= 60)).map((item) => item.name);
+  
+  return arrBooks;
+};
+console.log(oldBooks());
+
+//Pode-se resolver utilizando new Date().getFullYear(), como no exemplo do gabarito:
+/* function oldBooks() {
+  const currentYear = new Date().getFullYear();
+  return books
+    .filter((book) => currentYear - book.releaseYear > 60)
+    .map((book) => book.name);
+} */
