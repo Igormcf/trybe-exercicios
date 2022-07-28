38b82daf12efe10edcff491173aa3b644ad3fbc1
Igormcf <igormaciel82@gmail.com>
2022-07-28 18:20:45 -0300

    Finaliza exercicio 3

---
+++ modulo3_Back_End/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/exe3.1.ts
@@ -0,0 +1,22 @@
+import IPizza from "./exe3";
+
+const calabresa: IPizza = {
+  flavor: 'Calabresa',
+  slices: 8,
+}
+
+console.log(calabresa);
+
+const marguerita: IPizza = {
+  flavor: 'Marguerita',
+  slices: 6,
+}
+
+console.log(marguerita);
+
+const nutela: IPizza = {
+  flavor: 'Nutela',
+  slices: 4,
+}
+
+console.log(nutela);
